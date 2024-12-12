d9f74f47d7f31710be556c2aeaa0b4e310c10709
mariusz.matkowski <mariusz.matkowski@yahoo.com>
2024-12-12 15:22:15 +0100

    Basic logging functionality

--- include/Logger/Logger.hpp
+++ include/Logger/Logger.hpp
@@ -1,6 +1,73 @@
 #pragma once
 
+#include <cassert>
+#include <iostream>
+#include <ostream>
+#include <stdexcept>
+
+namespace logger {
 
 class Logger {
-      
+private:
+    class LoggerProxy {
+    public:
+        explicit LoggerProxy(std::ostream& os) : os_(os) {
+        }
+
+        template <typename T>
+        LoggerProxy& operator<<(const T& x) {
+            os_ << x;
+            return *this;
+        }
+
+    private:
+        std::ostream& os_;
+    };
+
+public:
+    enum class Severity {
+        Debug,
+        Info,
+        Err,
+        Warn,
+    };
+
+    explicit Logger(std::string name, std::ostream& os = std::cout)
+        : name_(std::move(name)), os_(os) {
+    }
+
+    template <typename T>
+    void operator<<(const T&) {
+        static_assert(false, "Invalid type passed to Logger::operator<<");
+    }
+
+    constexpr LoggerProxy operator<<(Logger::Severity severity) {
+        os_ << name_ << '/' << to_string(severity) << ": ";
+        return LoggerProxy{os_};
+    }
+
+private:
+    constexpr const char* to_string(Severity s) {
+        switch (s) {
+            case Severity::Debug:
+                return "debug";
+            case Severity::Info:
+                return "info";
+            case Severity::Err:
+                return "error";
+            case Severity::Warn:
+                return "warning";
+        }
+        return "unknown";
+    }
+
+    std::string name_;
+    std::ostream& os_;
 };
+
+inline constexpr Logger::Severity debug{Logger::Severity::Debug};
+inline constexpr Logger::Severity info{Logger::Severity::Info};
+inline constexpr Logger::Severity err{Logger::Severity::Err};
+inline constexpr Logger::Severity warn{Logger::Severity::Warn};
+
+} // namespace logger
--- tests/LoggerTests.cpp
+++ tests/LoggerTests.cpp
@@ -1,7 +1,62 @@
 #include <gtest/gtest.h>
 
+#include <fstream>
+#include <sstream>
+
 #include "Logger/Logger.hpp"
 
-TEST(LoggerTests, canCreateLogger) {
-    Logger l{};
+TEST(LoggerTest, canCreateLoggerWithGivenName) {
+    logger::Logger logger{"Test"};
 }
+
+TEST(LoggerTest, canCreateLoggerWithStringStream) {
+    std::stringstream ss;
+    logger::Logger logger{"Test", ss};
+}
+
+TEST(LoggerTest, canCreateLoggerWithFileStream) {
+    std::ofstream file{};
+    logger::Logger logger{"Test", file};
+}
+
+TEST(LoggerTest, canLogInfo) {
+    std::stringstream ss;
+    logger::Logger logger{"Test", ss};
+
+    logger << logger::info << "info";
+
+    EXPECT_EQ(ss.str(), "Test/info: info");
+}
+
+TEST(LoggerTest, canLogWarning) {
+    std::stringstream ss;
+    logger::Logger logger{"Test", ss};
+
+    logger << logger::warn << "warning";
+
+    EXPECT_EQ(ss.str(), "Test/warning: warning");
+}
+
+TEST(LoggerTest, canLogDebug) {
+    std::stringstream ss;
+    logger::Logger logger{"Test", ss};
+
+    logger << logger::debug << "debug";
+    EXPECT_EQ(ss.str(), "Test/debug: debug");
+}
+
+TEST(LoggerTest, canLogError) {
+    std::stringstream ss;
+    logger::Logger logger{"Test", ss};
+
+    logger << logger::err << "error";
+
+    EXPECT_EQ(ss.str(), "Test/error: error");
+}
+
+// TEST(LoggerTest, ssss) {
+//     std::stringstream ss;
+//     logger::Logger logger{"Test", ss};
+//
+//     logger << 1;
+// }
